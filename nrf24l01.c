954d679454a7e7b4f9b775f76c5a2ac34bed55cd
phonght32 <thanhphongho1998@gmail.com>
2024-02-11 12:52:22 +0700

    add initial parameters

--- nrf24l01.c
+++ nrf24l01.c
@@ -1,1 +1,7 @@
 #include "nrf24l01.h"
+
+typedef struct nrf24l01 {
+	nrf24l01_data_rate_t 		data_rate;		/*!< Data rate */
+	nrf24l01_output_pwr_t 		output_pwr;		/*!< Output power */
+	nrf24l01_mode_t 			mode;			/*!< Mode operation */
+} nrf24l01_t;
--- nrf24l01.h
+++ nrf24l01.h
@@ -27,6 +27,45 @@
 extern "C" {
 #endif
 
+#include "err_code.h"
+
+typedef struct nrf24l01* nrf24l01_handle_t;
+
+/**
+ * @brief   Data rate.
+ */
+typedef enum {
+	NRF24L01_DATA_RATE_250Kbps = 0,				/*!< Data rate 250 Kbps */
+	NRF24L01_DATA_RATE_1Mbps,					/*!< Data rate 1 Mbps */
+	NRF24L01_DATA_RATE_2Mbps,					/*!< Data rate 2 Mbps */
+} nrf24l01_data_rate_t;
+
+/**
+ * @brief   Output power.
+ */
+typedef enum {
+	NRF24L01_OUTPUT_PWR_0dBm = 0,				/*!< Output power 0 dBm */
+	NRF24L01_OUTPUT_PWR_6dBm,					/*!< Output power 6 dBm */
+	NRF24L01_OUTPUT_PWR_12dBm,					/*!< Output power 12 dBm */
+	NRF24L01_OUTPUT_PWR_18dBm					/*!< Output power 18 dBm */
+} nrf24l01_output_pwr_t;
+
+/**
+ * @brief   Mode.
+ */
+typedef enum {
+	NRF24L01_MODE_TRANSMITTER = 0,				/*!< Mode transmitter */
+	NRF24L01_MODE_RECEIVER						/*!< Mode receiver */
+} nrf24l01_mode_t;
+
+/**
+ * @brief   Configuration structure.
+ */
+typedef struct {
+	nrf24l01_data_rate_t 		data_rate;		/*!< Data rate */
+	nrf24l01_output_pwr_t 		output_pwr;		/*!< Output power */
+	nrf24l01_mode_t 			mode;			/*!< Mode operation */
+} nrf24l01_cfg_t;
 
 #ifdef __cplusplus
 }
